70b4834f25340afe8e6eb0ed85e828efb21c725c
RomanFdez <a.roman.fernandez@gmail.com>
2026-06-11 17:46:45 +0200

    feat(finanzas): FinanzasContext con listener finTransactions y CRUD (web)

---
+++ src/context/FinanzasContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { db } from '../firebaseConfig';
+import { collection, doc, setDoc, deleteDoc, onSnapshot } from 'firebase/firestore';
+
+const FinanzasContext = createContext();
+const COLLECTION = 'finTransactions';
+
+export const FinanzasProvider = ({ children }) => {
+  const [finTransactions, setFinTransactions] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    const unsub = onSnapshot(collection(db, COLLECTION), (snap) => {
+      const data = snap.docs.map(d => d.data());
+      data.sort((a, b) => (a.fecha < b.fecha ? 1 : a.fecha > b.fecha ? -1 : 0));
+      setFinTransactions(data);
+      setLoading(false);
+    });
+    return () => unsub();
+  }, []);
+
+  const addFin = async (t) => {
+    const id = t.id || uuidv4();
+    await setDoc(doc(db, COLLECTION, id), { ...t, id });
+  };
+
+  const updateFin = async (t) => {
+    await setDoc(doc(db, COLLECTION, t.id), t, { merge: true });
+  };
+
+  const removeFin = async (id) => {
+    await deleteDoc(doc(db, COLLECTION, id));
+  };
+
+  return (
+    <FinanzasContext.Provider value={{ finTransactions, loading, addFin, updateFin, removeFin }}>
+      {children}
+    </FinanzasContext.Provider>
+  );
+};
+
+export const useFinanzas = () => useContext(FinanzasContext);
